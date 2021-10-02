cac24774cfccfaba8ed40634d695222b57fee659
holiarcher <nhoj1nhoj2@gmail.com>
2021-10-02 15:18:07 +0800

    products_try_4

--- products.py
+++ products.py
@@ -1,4 +1,15 @@
+#读取挡案
 products = []
+with open('product.csv','r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,价格' in line:
+			continue
+			
+		s = line.strip().split(',')
+		products.append(s)
+		print(s)
+
+products = (products)
 
 while True:
 	name = input('请输入商品名称:')
@@ -12,6 +23,7 @@ while True:
 
 print(products)
 
-with open('product.csv','w') as f:
+with open('product.csv','w', encoding = 'utf-8') as f:
+	f.write('商品,价格\n')
 	for p in products:
 		f.write(p[0]+','+p[1]+'\n')
