c96a5e660aa76c2dfb220ee3f3c314d0dcaa65c7
holiarcher <nhoj1nhoj2@gmail.com>
2021-10-02 14:39:39 +0800

    products_try_3

--- products.py
+++ products.py
@@ -4,8 +4,14 @@ while True:
 	name = input('请输入商品名称:')
 	if name == 'q':
 		break
-
-	products.append(name)
+	price = input('请输入价格:')
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)
 
 print(products)
 
+with open('product.csv','w') as f:
+	for p in products:
+		f.write(p[0]+','+p[1]+'\n')
